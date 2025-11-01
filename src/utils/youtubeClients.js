cfaa4e016888360ffc9a98a744de3da566fed730
Anton <anton@example.com>
2025-11-01 12:55:14 +0200

    fix: widen youtube extractor clients

--- src/utils/youtubeClients.js
+++ src/utils/youtubeClients.js
@@ -1,34 +1,12 @@
 exports.CLIENTS = [
-    {
-        name: 'ANDROID_SDKLESS',
-        payload: {
-            clientName: 'ANDROID',
-            clientVersion: '19.44.38',
-            platform: 'MOBILE',
-            osName: 'Android',
-            osVersion: '11',
-            androidSdkVersion: 30
-        }
-    },
-    {
-        name: 'IOS',
-        payload: {
-            clientName: 'IOS',
-            clientVersion: '19.45.4',
-            deviceMake: 'Apple',
-            deviceModel: 'iPhone16,2',
-            platform: 'MOBILE',
-            osName: 'IOS',
-            osVersion: '17.5.1',
-            hl: 'en',
-            gl: 'US'
-        }
-    },
-    {
-        name: 'WEB_REMIX',
-        payload: {
-            clientName: 'WEB_REMIX',
-            clientVersion: '1.20241015.01.00'
-        }
-    }
+    'ios',
+    'web_embedded',
+    'web_safari',
+    'web',
+    'tv_embedded'
+];
+
+exports.FALLBACK_CLIENTS = [
+    'android_critical',
+    'android'
 ];
