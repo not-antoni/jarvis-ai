e05d9f2e7716f9d3315a228cf8852361cf953b71
Tony <tony@jarvis.ai>
2026-01-04 19:41:38 +0200

    Remove /leave command - bot stays in VC permanently

--- src/commands/music/index.js
+++ src/commands/music/index.js
@@ -5,10 +5,9 @@ const resume = require('./resume');
 const stop = require('./stop');
 const queue = require('./queue');
 const loop = require('./loop');
-const leave = require('./leave');
 
 // Lavalink removed - using yt-dlp for music playback
-const commandList = [play, skip, pause, resume, stop, queue, loop, leave];
+const commandList = [play, skip, pause, resume, stop, queue, loop];
 const commandMap = new Map(commandList.map(command => [command.data.name, command]));
 
 module.exports = {
