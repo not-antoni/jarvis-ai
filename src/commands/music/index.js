042ecd53ae4f78f21c1dc14d13e528f756dd7df2
Tony <tony@jarvis.ai>
2026-01-04 19:40:52 +0200

    Change /stop to stay in VC, add /leave command to disconnect

--- src/commands/music/index.js
+++ src/commands/music/index.js
@@ -5,9 +5,10 @@ const resume = require('./resume');
 const stop = require('./stop');
 const queue = require('./queue');
 const loop = require('./loop');
+const leave = require('./leave');
 
 // Lavalink removed - using yt-dlp for music playback
-const commandList = [play, skip, pause, resume, stop, queue, loop];
+const commandList = [play, skip, pause, resume, stop, queue, loop, leave];
 const commandMap = new Map(commandList.map(command => [command.data.name, command]));
 
 module.exports = {
