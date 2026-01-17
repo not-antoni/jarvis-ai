759cdbda922f39dfd8c8350653ba5481a066f9f7
Tony <105501167+not-antoni@users.noreply.github.com>
2026-01-17 09:13:09 +0200

    Add Google Analytics to all pages, fix nginx http2, create html-utils.js

--- routes/landing.js
+++ routes/landing.js
@@ -10,6 +10,14 @@ const LANDING_PAGE = `
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-7P8W1MN168"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-7P8W1MN168');
+    </script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jarvis - The Discord AI with Actual Personality | All-in-One Bot</title>
