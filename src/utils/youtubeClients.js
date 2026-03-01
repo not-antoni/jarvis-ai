be1d17d717ba46c600c8c19fa90b34c3ad8476b1
not-antoni <not-antoni@users.noreply.github.com>
2026-03-01 19:29:39 +0200

    Fix music startup latency, refresh docs, and remove root media assets

--- src/utils/youtubeClients.js
+++ src/utils/youtubeClients.js
@@ -1,3 +1,3 @@
-exports.CLIENTS = ['ios', 'web_embedded', 'web_safari', 'web', 'tv_embedded'];
+exports.CLIENTS = ['web', 'ios'];
 
-exports.FALLBACK_CLIENTS = ['android_critical', 'android'];
+exports.FALLBACK_CLIENTS = ['android'];
